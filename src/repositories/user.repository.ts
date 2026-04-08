347af663d4ec1e045e41a1851ba5cfd0499f0dca
Amir-Shaban32 <amirshaban973@gmail.com>
2026-04-08 18:12:04 +0200

    feat(repositories): add data access layer for users

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,66 @@
+import { prisma } from '../lib/prisma';
+import { Prisma , User_Role} from '../../generated/prisma';
+
+export class UserRepository {
+
+    async create(data:{username:string ,email:string ,role?:string, password:string}){
+        return await prisma.user.create({
+            data:{
+                username:data.username,
+                email:data.email,
+                password:data.password,
+                role:data.role as User_Role|| User_Role.USER
+            }
+        });
+    }
+
+    async findById(id:string){
+        return await prisma.user.findUnique({
+            where:{id}
+        });
+    }
+
+    async findByUsername(username:string){
+        return await prisma.user.findUnique({
+            where:{username}
+        });
+    }
+
+    async findByEmail(email:string){
+        return await prisma.user.findUnique({
+            where:{email}
+        })
+    }
+
+    async findAll(page:number=1 , limit:number=10){
+        const skip = (page-1) * limit
+        return await prisma.user.findMany({
+            skip,
+            take:limit,
+            select:{
+                username:true,
+                role:true,
+                created_at:true
+            }
+        });
+    }
+
+    async update(id:string , data:Prisma.UserUpdateInput){
+        return await prisma.user.update({
+            where:{id},
+            data,
+        });
+    }
+
+    async delete(id:string){
+        return await prisma.user.delete({
+            where:{id}
+        });
+    }
+
+    async count() {
+        return await prisma.user.count();
+    }
+
+}
+export const userRepository = new UserRepository();
