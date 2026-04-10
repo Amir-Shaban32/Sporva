b79045fabd49840c9f45c08f78ff0534158cd924
Amir-Shaban32 <amirshaban973@gmail.com>
2026-04-10 23:03:41 +0200

    feat(repository): implement match, match_events, and match_referee repositories with schedule, findByTeam, findByStatus, assign/unAssign referee, and event logging operations

---
+++ src/repositories/match.repository.ts
@@ -0,0 +1,93 @@
+import { prisma } from "../lib/prisma";
+import { Competitions, Match_status, Prisma } from "../../generated/prisma";
+import { Imatch } from "../types/match.type";
+
+class MatchRepository {
+  async schedule(data: Imatch) {
+    const year = new Date().getFullYear();
+    const season_matching = `${year}-${year + 1}`;
+    return await prisma.matches.create({
+      data: {
+        season: data.season ?? season_matching,
+        round: data.round,
+        stadium: data.stadium,
+        host_team_id: data.host_team_id,
+        guest_team_id: data.guest_team_id,
+        match_time: data.match_time,
+        competition: data.competition,
+        status: data.status,
+        host_team_score: data.host_team_score,
+        guest_team_score: data.guest_team_score,
+        got_extra_time: data?.got_extra_time,
+        got_penalties: data?.got_penalties,
+        host_penalty_score: data?.host_penalty_score,
+        guest_penalty_score: data?.guest_penalty_score,
+      },
+    });
+  }
+
+  async findByTeam(team_id: string) {
+    return await prisma.matches.findMany({
+      where: {
+        OR: [{ host_team_id: team_id }, { guest_team_id: team_id }],
+      },
+    });
+  }
+
+  async findLive() {
+    return await prisma.matches.findMany({
+      where: { status: "LIVE" },
+    });
+  }
+
+  async findByStatus(status: Match_status) {
+    return await prisma.matches.findMany({
+      where: { status },
+    });
+  }
+
+  async findByComp(competition: Competitions) {
+    return await prisma.matches.findMany({
+      where: { competition },
+    });
+  }
+
+  async findByDate(date: Date) {
+    return await prisma.matches.findMany({
+      where: { match_time: date },
+    });
+  }
+
+  async findByRound(round: number) {
+    return await prisma.matches.findMany({
+      where: { round },
+    });
+  }
+
+  async findBySeason(season: string) {
+    return await prisma.matches.findMany({
+      where: { season },
+    });
+  }
+
+  async findGotExtra() {
+    return await prisma.matches.findMany({
+      where: { got_extra_time: true },
+    });
+  }
+
+  async findGotPenalties() {
+    return await prisma.matches.findMany({
+      where: { got_penalties: true },
+    });
+  }
+
+  async update(id: string, data: Prisma.UserUpdateInput) {
+    return await prisma.matches.update({
+      where: { id },
+      data: data,
+    });
+  }
+}
+
+export const matchRepository = new MatchRepository();
