5bf8c183d7f408df01a16c2bf0dbc7b10e9b8db0
Amir-Shaban32 <amirshaban973@gmail.com>
2026-04-10 23:03:46 +0200

    feat(repository): implement transfer repository with create, findByPlayer, and findByTeam operations

---
+++ src/repositories/transfer.repository.ts
@@ -0,0 +1,33 @@
+import { prisma } from "../lib/prisma";
+import { Itransfer } from "../types/transfer.type";
+
+class TransferRepository {
+  async create(data: Itransfer) {
+    return await prisma.transfers.create({
+      data: {
+        player_id: data.player_id,
+        from_team_id: data.from_team_id,
+        to_team_id: data.to_team_id,
+        transfer_date: data.transfer_date ?? new Date(),
+        transfer_fee: data.transfer_fee,
+        transfer_type: data.transfer_type,
+      },
+    });
+  }
+
+  async findByPlayer(player_id: string) {
+    return await prisma.transfers.findMany({
+      where: { player_id },
+    });
+  }
+
+  async findByteam(team_id: string) {
+    return await prisma.transfers.findMany({
+      where: {
+        OR: [{ from_team_id: team_id }, { to_team_id: team_id }],
+      },
+    });
+  }
+}
+
+export const transferREpository = new TransferRepository();
