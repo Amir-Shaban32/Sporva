d8d6a49af4bb68ce4bf2a93d284e2e47d314021d
Amir-Shaban32 <amirshaban973@gmail.com>
2026-04-12 21:29:20 +0200

    feat(validations): add Zod create schema validate req.body on POST request

---
+++ src/validations/transfer.validation.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import { Transfer_type } from "../../generated/prisma";
+
+export const createTransferValidation = z.object({
+  player_id: z.cuid(),
+  from_team_id: z.cuid(),
+  to_team_id: z.cuid(),
+  transfer_date: z.coerce.date().optional(),
+  transfer_fee: z.number().positive("Transfer fee must be positive"),
+  transfer_type: z.enum([
+    Transfer_type.permanent,
+    Transfer_type.free,
+    Transfer_type.loan,
+  ]),
+});
+
+export type CreateTransferInput = z.infer<typeof createTransferValidation>;
