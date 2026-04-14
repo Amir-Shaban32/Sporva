d98d8904043939c3b9ed6a25409237729170d7ed
Amir-Shaban32 <amirshaban973@gmail.com>
2026-04-14 20:55:33 +0200

    feat(auth): add register service with duplicate check and password hashing before storing in DB

---
+++ src/services/auth/register.service.ts
@@ -0,0 +1,32 @@
+import { hashPassword } from "../../utils/password";
+import { createUserService, getUserByUsernameService } from "../users.service";
+import { ServiceResult, ICreateUser, IUser } from "../../types";
+
+export const registerService = async (
+  data: ICreateUser,
+): Promise<ServiceResult<IUser>> => {
+  try {
+    const existing = await getUserByUsernameService(data.username);
+    if (existing.success)
+      return {
+        success: false,
+        error: "User already exists!",
+        code: "CONFLICT",
+      };
+
+    const hashedPassword = await hashPassword(data.password);
+    const userData = { ...data, password: hashedPassword };
+
+    const user = await createUserService(userData);
+    if (!user.success)
+      return {
+        success: false,
+        error: user.error,
+        code: user.code ?? "DB_ERROR",
+      };
+
+    return { success: true, data: user.data };
+  } catch (error) {
+    return { success: false, code: "DB_ERROR", error: "Database error" };
+  }
+};
