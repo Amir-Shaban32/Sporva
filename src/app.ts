71031c84411fd802edb29402cb11e9e0ea79431c
Amir-Shaban32 <amirshaban973@gmail.com>
2026-04-15 11:18:56 +0200

    refactor(app): update imports to kebab-case file naming

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,13 @@ import transferRouter from "./routes/transfer.route";
 import teamRouter from "./routes/team.route";
 import refereeRouter from "./routes/referee.route";
 import playerRouter from "./routes/player.route";
-import playerContractRouter from "./routes/player_contract.route";
+import playerContractRouter from "./routes/player-contract.route";
 import matchRouter from "./routes/manager.route";
-import matchRefereeRouter from "./routes/match_referee.route";
-import matchEventRouter from "./routes/match_event.route";
+import matchRefereeRouter from "./routes/match-referee.route";
+import matchEventRouter from "./routes/match-event.route";
 import managerRouter from "./routes/manager.route";
-import managerContractRouter from "./routes/manager_contract.route";
-import leagueStandingsRouter from "./routes/league_standing.route";
+import managerContractRouter from "./routes/manager-contract.route";
+import leagueStandingsRouter from "./routes/league-standing.route";
 import authRouter from "./routes/auth/auth.route";
 
 const app: Application = express();
